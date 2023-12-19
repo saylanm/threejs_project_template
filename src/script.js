7c68d90e797418235c23903fdc84530d8a83556d
Семён Семёнов <semyon_semyonov_2003@bk.ru>
2023-12-19 20:01:57 +0400

    review1

--- src/script.js
+++ src/script.js
@@ -1,48 +1,56 @@
 import * as THREE from 'three';
-
 import init from './init';
-
 import './style.css';
 
 const { sizes, camera, scene, canvas, controls, renderer } = init();
 
 camera.position.z = 3;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.SphereGeometry(1, 32, 32);
 const material = new THREE.MeshBasicMaterial({
-	color: 'gray',
-	wireframe: true,
+  color: 'green',
+  wireframe: true,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+const container = document.querySelector('.container');
+container.appendChild(canvas);
+
 const tick = () => {
-	controls.update();
-	renderer.render(scene, camera);
-	window.requestAnimationFrame(tick);
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
 };
 tick();
 
-/** Базовые обпаботчики событий длы поддержки ресайза */
+function changeColor(color) {
+  mesh.material.color.set(color);
+  renderer.render(scene, camera);
+}
+
+const colorButton = document.getElementById('color-button');
+colorButton.addEventListener('click', () => {
+  const randomColor = new THREE.Color(Math.random(), Math.random(), Math.random());
+  changeColor(randomColor);
+});
+
 window.addEventListener('resize', () => {
-	// Обновляем размеры
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-	// Обновляем соотношение сторон камеры
-	camera.aspect = sizes.width / sizes.height;
-	camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-	// Обновляем renderer
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-	renderer.render(scene, camera);
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
 });
 
 window.addEventListener('dblclick', () => {
-	if (!document.fullscreenElement) {
-		canvas.requestFullscreen();
-	} else {
-		document.exitFullscreen();
-	}
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
 });
